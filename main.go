1c333c10fa5d2abf62f83a154abae340cf1a3ab7
Andrew Pruski <dbafromthecold@gmail.com>
2024-03-21 14:01:06 +0000

    updating code to handle network ns and port forwarding

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"syscall"
 )
 
-// go run main.go run <cmd> <args>
 func main() {
 	switch os.Args[1] {
 	case "run":
@@ -23,23 +24,23 @@ func main() {
 }
 
 func run() {
-	fmt.Printf("Running %v \n", os.Args[2:])
+	fmt.Printf("Running %v as parent\n", os.Args[2:])
+
+	setupNetworkNamespace()
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET, // Added CLONE_NEWNET for network namespace
-		Unshareflags: syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET,
 	}
 
 	must(cmd.Run())
-	setupPortForwarding() // This function will need to be defined to set up port forwarding
 }
 
 func child() {
-	fmt.Printf("Running %v \n", os.Args[2:])
+	fmt.Printf("Running %v as child\n", os.Args[2:])
 
 	cg()
 
@@ -49,13 +50,51 @@ func child() {
 	cmd.Stderr = os.Stderr
 
 	must(syscall.Sethostname([]byte("container")))
-	must(syscall.Chroot("/home/dbafromthecold/sqlserver"))
+	must(syscall.Chroot("/your/new/root"))
 	must(os.Chdir("/"))
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+	setupVethForNamespace()
+
 	must(cmd.Run())
 
 	must(syscall.Unmount("proc", 0))
 }
+
+func setupNetworkNamespace() {
+    // Create a new network namespace
+    err := exec.Command("ip", "netns", "add", "dbafromthecold-ns").Run()
+    must(err)
+
+    // TODO - add check to make sure namespace does not already exist
+}
+
+
+func setupVethForNamespace() {
+
+    // Create a veth pair: one end (veth-host) stays in the host namespace,
+    // the other end (veth-ns) is moved to "dbafromthecold-ns".
+    must(exec.Command("ip", "link", "add", "veth-host", "type", "veth", "peer", "name", "veth-ns").Run())
+
+    // Move veth-ns to the namespace
+    must(exec.Command("ip", "link", "set", "veth-ns", "netns", "dbafromthecold-ns").Run())
+
+    // Assign an IP to veth-host in the host namespace
+    must(exec.Command("ip", "addr", "add", "192.168.1.1/24", "dev", "veth-host").Run())
+    must(exec.Command("ip", "link", "set", "veth-host", "up").Run())
+
+    // Assign an IP to veth-ns in "mynamespace" and bring it up
+    must(exec.Command("ip", "netns", "exec", "dbafromthecold-ns", "ip", "addr", "add", "192.168.1.2/24", "dev", "veth-ns").Run())
+    must(exec.Command("ip", "netns", "exec", "dbafromthecold-ns", "ip", "link", "set", "veth-ns", "up").Run())
+    must(exec.Command("ip", "netns", "exec", "dbafromthecold-ns", "ip", "route", "add", "default", "via", "192.168.1.1").Run())
+}
+
+func setupPortForwarding() {
+	// Forward traffic from port 15789 on the host to port 1433 in the namespace
+	must(exec.Command("iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp", "--dport", "15789", "-j", "DNAT", "--to-destination", "192.168.1.2:1433").Run())
+	must(exec.Command("iptables", "-t", "nat", "-A", "POSTROUTING", "-j", "MASQUERADE").Run())
+}
+
 
 func cg() {
 	cgroups := "/sys/fs/cgroup/"
@@ -79,12 +118,4 @@ func must(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func setupPortForwarding() {
-	// Here you would use exec.Command to call iptables or an equivalent command to forward ports.
-	// Example (you'll need to adjust the iptables command to fit your needs):
-	// err := exec.Command("iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp", "--dport", "15789", "-j", "DNAT", "--to-destination", "IP_OF_NAMESPACE:1433").Run()
-	// must(err)
-	// Note: You'll need to replace IP_OF_NAMESPACE with the actual IP address of your network namespace.
 }
